dfeba03cd6896e99fae6060f33f8ad82631bc8a4
LanzP22 <123591709+LanzPPPP@users.noreply.github.com>
2023-11-05 15:31:30 +0800

    Update rasa-db.js

--- routes/rasa-db.js
+++ routes/rasa-db.js
@@ -1,15 +1,15 @@
 const sql = require("mysql2");
 const dotenv = require("dotenv").config();
-/*const db1 = sql.createConnection({
+const db1 = sql.createConnection({
     host: process.env.DATABASE_HOST1,
     user: process.env.DATABASE_USER1,
     password: process.env.DATABASE_PASSWORD1,
     database: process.env.DATABASE1
-})*/
-const db1 = sql.createConnection({
+})
+/*const db1 = sql.createConnection({
     host: process.env.DATABASE_HOST1,
     user: process.env.DATABASE_USER1,
     password: process.env.DATABASE_PASSWORD1
-})
+})*/
 
 module.exports = db1;
