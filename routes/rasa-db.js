f5df45732c737b84a0de22e8192b888fd472425d
LanzP22 <123591709+LanzPPPP@users.noreply.github.com>
2023-11-05 15:30:29 +0800

    d

--- routes/rasa-db.js
+++ routes/rasa-db.js
@@ -11,34 +11,5 @@ const db1 = sql.createConnection({
     user: process.env.DATABASE_USER1,
     password: process.env.DATABASE_PASSWORD1
 })
-db1.query(`SHOW DATABASES LIKE '${process.env.DATABASE1}';`, (error, result) => 
-{
-    console.log("A");
-    if (error || result.length == 0)
-    {
-        console.log("B");
-        db1.query(`CREATE DATABASE ${process.env.DATABASE1}`, (error, result) => {
-            db1.changeUser({database: process.env.DATABASE1}, (error) => {
-                createTablesIfNotExist();
-            });
-        });
-    }
-    else
-    {
-        db1.changeUser({database: process.env.DATABASE1}, (error) => {
-            createTablesIfNotExist();
-        });
-    }
-});
-
-function createTablesIfNotExist()
-{
-    db1.query("SELECT * FROM archieved_inputted_table2", (error, result) => {
-        if (error)
-        {
-            db1.query("CREATE TABLE archieved_inputted_table2(rasa_id int(11) NOT NULL, full_name varchar(255) NOT NULL, event_day date NOT NULL, event_name varchar(255) NOT NULL, event_description varchar(255) NOT NULL, start_time time NOT NULL, end_time time NOT NULL, rasa_status varchar(255) NOT NULL);");
-        }
-    });
-}
 
 module.exports = db1;
